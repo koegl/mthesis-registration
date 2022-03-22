187c4a8924d9673316affaf4ddd1f4e42f22aced
Fryderyk Kögl <koeglfryderyk@gmail.com>
2022-03-22 11:53:15 -0400

    renamed parameters and used dummy mr and us images

--- main.py
+++ main.py
@@ -15,25 +15,17 @@ def plot(array):
 
 def main():
     # load images
-    # phantom = Image.open("/Users/fryderykkogl/Documents/university/master/thesis/code/mthesis-registration/phantom.png")
-    # phantom = np.asarray(phantom).astype('float64')
-    # normal phantom
-    with open("/Users/fryderykkogl/Downloads/similarity_LC2/p.pkl", 'rb') as f:
-        p = pickle.load(f)
-    # phantom with noise
-    with open("/Users/fryderykkogl/Downloads/similarity_LC2/img1.pkl", 'rb') as f:
-        img1 = pickle.load(f)
+    mr = Image.open("/Users/fryderykkogl/Documents/university/master/thesis/code/mthesis-registration/misc/test_mr.png")
+    mr = np.asarray(mr).astype('float64') / 255
+    us = Image.open("/Users/fryderykkogl/Documents/university/master/thesis/code/mthesis-registration/misc/test_us.png")
+    us = np.asarray(us).astype('float64') / 255
 
-    img2 = np.zeros((p.shape[0], p.shape[1], 2))
-    img2[:, :, 0] = p
-    # phantom2[:, :, 1] = np.absolute(ndimage.sobel(p, axis=1))  # this gives similar results to matlab gradient (!=)
+    # create a combined image of the MR and MR-gradient
+    mr_and_grad = np.zeros((us.shape[0], us.shape[1], 2))
+    mr_and_grad[:, :, 0] = mr
+    mr_and_grad[:, :, 1] = np.absolute(ndimage.sobel(mr, axis=1))  # matlab gradient is a bit different
 
-    # load gradient
-    with open("/Users/fryderykkogl/Downloads/similarity_LC2/img2_second_layer.pkl", 'rb') as f:
-        img2_second_layer = pickle.load(f)
-    img2[:, :, 1] = img2_second_layer
-
-    similarity, measure, weight = lc2_similarity(img1, img2)
+    similarity, measure, weight = lc2_similarity(us, mr_and_grad)
 
     print(f"{similarity=}\n{measure=},\n{weight=}")
 
