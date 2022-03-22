527f69c0e24c57c4d357474b98430b1c37b1a16a
Fryderyk Kögl <koeglfryderyk@gmail.com>
2022-03-22 16:58:39 -0400

    similarity patchbased function complete

--- lc2_similarity.py
+++ lc2_similarity.py
@@ -58,3 +58,60 @@ def lc2_similarity(us, mr):
         measure = 0
 
     return similarity, measure, weight
+
+
+def lc2_similarity_patch(img1, img2, patchsize=9):
+    """
+    Calculates the LC2 similarity patch-wise between a 2D US image and the corresponding 2D MR image. The images have to
+    be of equal size. Based on http://campar.in.tum.de/Main/LC2Code (LC2SimilarityPatch)
+    :param img1: The US image (one channel image of size n*m)
+    :param img2: The MR image (one channel image of size n*m)
+    :param patchsize: Size of the patch
+    :return: similarity
+    """
+    # calculate gradient of MR
+    # create an MR+gradient matrix
+    mr_and_grad = np.zeros((img1.shape[0], img1.shape[1], 2))
+    mr_and_grad[:, :, 0] = img2
+    mr_and_grad[:, :, 1] = np.absolute(np.gradient(img2, axis=1))
+
+    img2 = mr_and_grad
+
+    # set parameters
+    max_x = img1.shape[0]
+    max_y = img1.shape[1]
+
+    # half of the maximal size of the patch
+    total_size = ((2*patchsize + 1)**2) / 2
+
+    sim = np.zeros(img1.shape)
+    measure = np.zeros(img1.shape)
+    weights = np.zeros(img1.shape)
+
+    # loop through all pixels
+    for y in range(max_y):
+        for x in range(max_x):
+
+            # extract patches from us and mr+grad
+            patch1 = img1[
+                            max(1, x-patchsize):min(max_x, x+patchsize),
+                            max(1, y-patchsize):min(max_y, y+patchsize)
+                     ]
+
+            patch2 = img2[
+                            max(1, x-patchsize):min(max_x, x+patchsize),
+                            max(1, y-patchsize):min(max_y, y+patchsize),
+                            :
+                     ]
+
+            # if a patch is bigger than half the maximal size of the patch calculate the similarity
+            # todo what happens with border pixels?
+            if patch1.size > total_size:
+                sim[x, y], measure[x, y], weights[x, y] = lc2_similarity(patch1, patch2)
+
+    if sum(weights.flatten()) == 0:
+        return 0
+
+    similarity = sum(measure.flatten()) / sum(weights.flatten())
+
+    return similarity
