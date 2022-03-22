588eeda34006284e5d7ce2823743bd6b55185513
Fryderyk Kögl <koeglfryderyk@gmail.com>
2022-03-22 10:55:31 -0400

    gives the same results as matlab code
    (the similarity function, in the main the gradient was different, so it and the phantom were loaded from matlab)

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from PIL import Image
 import cv2
 from scipy import ndimage
 import matplotlib.pyplot as plt
+import pickle
 
 
 def plot(array):
@@ -13,24 +14,26 @@ def plot(array):
 
 
 def main():
-    # load image
-    phantom = Image.open("/Users/fryderykkogl/Documents/university/master/thesis/code/mthesis-registration/phantom.png")
-    phantom = np.asarray(phantom).astype('float64')
+    # load images
+    # phantom = Image.open("/Users/fryderykkogl/Documents/university/master/thesis/code/mthesis-registration/phantom.png")
+    # phantom = np.asarray(phantom).astype('float64')
+    # normal phantom
+    with open("/Users/fryderykkogl/Downloads/similarity_LC2/p.pkl", 'rb') as f:
+        p = pickle.load(f)
+    # phantom with noise
+    with open("/Users/fryderykkogl/Downloads/similarity_LC2/img1.pkl", 'rb') as f:
+        img1 = pickle.load(f)
 
-    # add noise
-    # gauss_noise = np.random.normal(0, 1, phantom.size)
-    # gauss_noise = gauss_noise.reshape(phantom.shape[0], phantom.shape[1]) * 10
-    # phantom_noise = cv2.add(phantom, gauss_noise)
+    img2 = np.zeros((p.shape[0], p.shape[1], 2))
+    img2[:, :, 0] = p
+    # phantom2[:, :, 1] = np.absolute(ndimage.sobel(p, axis=1))  # this gives similar results to matlab gradient (!=)
 
-    phantom2 = np.zeros((phantom.shape[0], phantom.shape[1], 2))
-    phantom2[:, :, 0] = phantom
-    phantom2[:, :, 1] = np.absolute(ndimage.sobel(phantom, axis=1))
+    # load gradient
+    with open("/Users/fryderykkogl/Downloads/similarity_LC2/img2_second_layer.pkl", 'rb') as f:
+        img2_second_layer = pickle.load(f)
+    img2[:, :, 1] = img2_second_layer
 
-    plot(1-phantom)
-    plot(phantom2[:, :, 0])
-    plot(phantom2[:, :, 1])
-
-    similarity, measure, weight = lc2_similarity(1 - phantom, phantom2)
+    similarity, measure, weight = lc2_similarity(img1, img2)
 
     print(f"{similarity=}\n{measure=},\n{weight=}")
 
