35a7b09fd40561f49d8397767346f4906da363fe
Fryderyk Kögl <koeglfryderyk@gmail.com>
2022-03-23 08:56:38 -0400

    initial attempt at porting to tensorflow
    currently there's a proble with finding indices of non-zero elements

---
+++ main_tf.py
@@ -0,0 +1,38 @@
+import os.path
+import tensorflow as tf
+from lc2_similarity import lc2_similarity_patch
+
+import numpy as np
+from PIL import Image
+import argparse
+import pathlib
+
+
+def main(params):
+    # load images
+    mr = Image.open(params.mr_path)
+    mr = np.asarray(mr).astype('float64') / 255
+    mr = tf.convert_to_tensor(mr)
+    us = Image.open(params.ultrasound_path)
+    us = np.asarray(us).astype('float64') / 255
+    us = tf.convert_to_tensor(us)
+
+    similarity = lc2_similarity_patch(us, mr, params.patch_size)
+
+    print(f"{similarity=}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    current_directory = pathlib.Path(__file__).parent.resolve()
+
+    parser.add_argument("-up", "--ultrasound_path", default=os.path.join(current_directory, "misc/test_us.png"),
+                        help="Path to the Ultrasound image")
+    parser.add_argument("-mp", "--mr_path", default=os.path.join(current_directory, "misc/test_mr.png"),
+                        help="Path to the MR image")
+    parser.add_argument("-ps", "--patch_size", default=9, help="The patch size for calculating LC2")
+
+    args = parser.parse_args()
+
+    main(args)
