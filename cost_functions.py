32f9d8cbf6ed055b1202d361b331b9516fffe25e
Fryderyk Kögl <koeglfryderyk@gmail.com>
2022-03-25 10:58:19 -0400

    added affien trasnformation cost function

--- cost_functions.py
+++ cost_functions.py
@@ -1,5 +1,7 @@
-from similarity_metrics import mi, lc2
-from image_manipulation import rigid_transform
+from similarity_metrics import compute_similarity_metric ,ssd, ncc, mi, lc2
+from image_manipulation import rigid_transform, affine_transform
+
+# todo make one cost function that determines if it's affine or rigid based on the parameters/transform matrix
 
 
 def rigid_transformation_cost_function(transform_parameters, fixed_image, moving_image, similarity="lc2"):
@@ -22,11 +24,33 @@ def rigid_transformation_cost_function(transform_parameters, fixed_image, moving
     transformed_moving_image = rigid_transform(moving_image, angle, dx, dy)
 
     # compute the similarity value
-    if similarity.lower() == "lc2":
-        s = lc2(transformed_moving_image, fixed_image)
-    elif similarity.lower() == "mi":
-        s = -mi(transformed_moving_image, fixed_image)
-    else:
-        raise NotImplementedError("Wrong similarity metric. Only lc2 is implemented at the moment.")
+    s = compute_similarity_metric(transformed_moving_image, fixed_image, similarity)
+
+    return s
+
+
+def affine_transformation_cost_function(transform_parameters, fixed_image, moving_image, similarity="lc2"):
+    """
+    Cost function for a rigid transformation.
+    Computes a similarity measure between the given images using the given similarity metric
+    :param transform_parameters: angle, dx, dy
+    :param fixed_image: the reference image
+    :param moving_image: the image which will be transformed to match the reference image
+    :param similarity: the name of the similarity metric
+    :return: the computed similarity metric
+    """
+
+    # get parameters
+    angle = transform_parameters[0]
+    dx = transform_parameters[1]
+    dy = transform_parameters[2]
+    sx = transform_parameters[3]
+    sy = transform_parameters[4]
+
+    # transform the moving image with the parameters
+    transformed_moving_image = affine_transform(moving_image, angle, dx, dy, sx, sy)
+
+    # compute the similarity value
+    s = compute_similarity_metric(transformed_moving_image, fixed_image, similarity)
 
     return s
