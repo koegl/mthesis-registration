b8b838c1686311409a4b6be196ba6ecd0911dcc7
Fryderyk Kögl <koeglfryderyk@gmail.com>
2022-03-25 10:56:57 -0400

    added ssd and ncc and a functon to gather all of them

--- similarity_metrics.py
+++ similarity_metrics.py
@@ -4,6 +4,64 @@ import multiprocessing
 import sys
 
 
+def compute_similarity_metric(im1, im2, metric="ssd", patchsize=None):
+    """
+    Copute the given similarity metric between images m1 and m2
+    :param im1: First image
+    :param im2: Second image
+    :param metric: Third image
+    :param patchsize: Patchsize for LC2
+    :return: The calculated similarity
+    """
+
+    if metric.lower() == "ssd":
+        s = ssd(im1, im2)
+    elif metric.lower() == "ncc":
+        s = -ncc(im1, im2)  # we want to maximise it, so '-'
+    elif metric.lower() == "mi":
+        s = -mi(im1, im2)  # we want to maximise it, so '-'
+    elif metric.lower() == "lc2":
+        s = lc2(im1, im2)
+    else:
+        raise NotImplementedError("Wrong similarity metric. Only lc2 is implemented at the moment.")
+
+    return s
+
+
+def ssd(x, y):
+    """
+    Computes the sum of the squared differences between the given matrices
+    :param x: first image
+    :param y: second image
+    :return: sum of the squared differences
+    """
+
+    n, m = y.shape
+
+    result = (1/(n*m)) * np.sum(np.square(x - y))
+
+    return result
+
+
+def ncc(x, y):
+    """
+    Computes the normalised cross correlation between the given matrices
+    :param x: first image
+    :param y: second image
+    :return: normalised cross correlation
+    """
+    n, m = y.shape
+
+    # Demean the images
+    x -= np.mean(x)
+    y -= np.mean(y)
+
+    # calculate ncc
+    s = (1/(n*m)) * np.sum((x * y) / (x.std() * y.std()))
+
+    return s
+
+
 def mi(x, y):
     """
     Computes the mutual information between the given matrices
