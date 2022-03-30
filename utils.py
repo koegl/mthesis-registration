1927fed58a9580c07b7603ce1abf22badbe1ee36
Fryderyk Kögl <koeglfryderyk@gmail.com>
2022-03-30 11:26:04 -0400

    added new plot function

--- utils.py
+++ utils.py
@@ -3,27 +3,41 @@ import numpy as np
 import cv2
 
 
-def plot_images(x, y):
-
-    '''Function to display two images and their difference on screen.
-    :param x: first image to display
-    :param y: second image to display
-    :return: void
-    '''
+def plot_images(x, y, z):
+    """
+    Display two images and their difference
+    :param x: first image
+    :param y: second image
+    :param z: third image
+    """
 
     # Creating a figure with subplots of a certain size.
-    fig, (plot1, plot2, plot3) = plt.subplots(1, 3, figsize=(10, 3))
+    fig, ((plot1, plot2, plot3), (plot4, plot5, plot6)) = plt.subplots(2, 3, figsize=(10, 8))
 
     # Display the two images.
     plot1.imshow(x, cmap=plt.cm.gray)
     plot1.axis('off')
+    plot1.set_title("Fixed image")
+
     plot2.imshow(y, cmap=plt.cm.gray)
     plot2.axis('off')
+    plot2.set_title("Initial moving image")
 
-    # Computing the difference of the two images and display it.
-    diff = x - y
-    plot3.imshow(diff, cmap=plt.cm.gray)
+    plot3.imshow(x-y, cmap=plt.cm.gray)
     plot3.axis('off')
+    plot3.set_title("Initial overlap")
+
+    plot4.imshow(x, cmap=plt.cm.gray)
+    plot4.axis('off')
+    plot4.set_title("Fixed image")
+
+    plot5.imshow(z, cmap=plt.cm.gray)
+    plot5.axis('off')
+    plot5.set_title("Transformed moving image")
+
+    plot6.imshow(x-z, cmap=plt.cm.gray)
+    plot6.axis('off')
+    plot6.set_title("Final overlap")
 
     plt.show()
 
