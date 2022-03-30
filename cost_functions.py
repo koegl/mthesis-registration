a217a4bc93f10c1ba05453b33d2e4009584fa573
Fryderyk Kögl <koeglfryderyk@gmail.com>
2022-03-30 11:11:10 -0400

    added general cost function

--- cost_functions.py
+++ cost_functions.py
@@ -2,6 +2,27 @@ from image_manipulation import rigid_transform
 from similarity_metrics import compute_similarity_metric
 
 
+def cost_function(transform_parameters, fixed_image, moving_image, similarity="ssd"):
+    """
+    Cost function for several transformation types.
+    Computes a similarity measure between the given images using the given similarity metric
+    :param transform_parameters: the transformation parameters
+    :param fixed_image: the reference image
+    :param moving_image: the image which will be transformed to match the reference image
+    :param similarity: the name of the similarity metric
+    :return: the computed similarity metric
+    """
+
+    if len(transform_parameters) == 3:  # rotate + translate
+        s = rigid_transformation_cost_function(transform_parameters, fixed_image, moving_image, similarity)
+    elif len(transform_parameters) == 5:  # scale + rotate + translate
+        s = affine_transformation_cost_function(transform_parameters, fixed_image, moving_image, similarity)
+    else:
+        raise NotImplementedError("Wrong number of transformation parameters in cost_function()")
+
+    return s
+
+
 def rigid_transformation_cost_function(transform_parameters, fixed_image, moving_image, similarity="lc2"):
     """
     Cost function for a rigid transformation.
@@ -27,20 +48,29 @@ def rigid_transformation_cost_function(transform_parameters, fixed_image, moving
     return s
 
 
-def cost_function(transform_parameters, fixed_image, moving_image, similarity="ssd"):
+def affine_transformation_cost_function(transform_parameters, fixed_image, moving_image, similarity="lc2"):
     """
-    Cost function for several transformation types.
+    Cost function for a rigid transformation.
     Computes a similarity measure between the given images using the given similarity metric
-    :param transform_parameters: the transformation parameters
+    :param transform_parameters: angle, dx, dy
     :param fixed_image: the reference image
     :param moving_image: the image which will be transformed to match the reference image
     :param similarity: the name of the similarity metric
     :return: the computed similarity metric
     """
 
-    if len(transform_parameters) == 3:  # rotate + translate
-        s = rigid_transformation_cost_function(transform_parameters, fixed_image, moving_image, similarity)
-    else:
-        raise NotImplementedError("Wrong number of transformation parameters in cost_function()")
+    # get parameters
+    angle = transform_parameters[0]
+    dx = transform_parameters[1]
+    dy = transform_parameters[2]
+    sx = transform_parameters[3]
+    sy = transform_parameters[4]
+
+    # transform the moving image with the parameters
+    transformed_moving_image = affine_transform(moving_image, angle, dx, dy, sx, sy)
+
+    # compute the similarity value
+    s = compute_similarity_metric(transformed_moving_image, fixed_image, similarity)
 
     return s
+
