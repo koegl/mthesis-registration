0ad7ff160e7b5a542d3030de7904d13ea4d4ffa0
Fryderyk Kögl <koeglfryderyk@gmail.com>
2022-04-03 12:17:23 -0400

    added plot function to plot 2d slices of 3d volumes

--- utils.py
+++ utils.py
@@ -1,6 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import cv2
+import nibabel as nib
+from matplotlib.widgets import Slider
+import functools
 
 
 def plot_images(x, y=None, z=None, main_title=""):
@@ -12,11 +15,51 @@ def plot_images(x, y=None, z=None, main_title=""):
     :param main_title: The main title over all the sub-plots
     """
 
-    if y is None and z is None:
-        plt.imshow(x)
-        plt.show()
-        return
+    if len(x.shape) == 3:
+        plot_slice(x, main_title)
+    elif y is None and z is None:
+        plot_one_image(x, main_title)
 
+    elif x is not None and y is not None and z is not None:
+        plot_fixed_moving_overlap(x, y, z, main_title)
+
+    else:
+        raise NotImplementedError("wrong number of images or shapes")
+
+
+def plot_slice(volume, title=None):
+    """
+    Plot a 2d slice of a 3d volume
+    :param volume: the 3d volume
+    :param title: the title of the plot
+    """
+    image = plt.imshow(volume[0, :, :], cmap=plt.cm.gray)
+    plt.axis('off')
+    plt.title(title)
+    plt.subplots_adjust(bottom=0.25)
+
+    # make a horizontal slider to control the slice
+    slice_slider_ax = plt.axes([0.25, 0.1, 0.65, 0.03])
+    slice_slider = Slider(slice_slider_ax, label='Slice', valmin=0, valmax=volume.shape[0]-1, valinit=0, valstep=1)
+
+    # register the callback function with the slider
+    slice_slider.on_changed(functools.partial(update_slice, image, volume))
+
+    plt.show()
+
+
+def update_slice(image, volume, value):
+    image.set_data(volume[int(value), :, :])
+
+
+def plot_one_image(image, title=""):
+    plt.imshow(image, cmap=plt.cm.gray)
+    plt.axis('off')
+    plt.title(title)
+    plt.show()
+
+
+def plot_fixed_moving_overlap(x, y, z, main_title=""):
     # Creating a figure with subplots of a certain size.
     fig, ((plot1, plot2, plot3), (plot4, plot5, plot6)) = plt.subplots(2, 3, figsize=(10, 8))
 
