bf8f5849c70632a7ac822a922d3709651d449a3f
Fryderyk Kögl <koeglfryderyk@gmail.com>
2022-04-13 19:11:05 -0400

    fixed perspective trasnform and storing true registratoin too

--- main.py
+++ main.py
@@ -22,9 +22,9 @@ def main(params):
     optimiser = "scipy"
 
     # Give some initial values to the transformation parameters
-    perspective_transform = np.eye(3, dtype=np.float64) - 0.002 * np.random.randn(3, 3)
-    # perspective_transform[0, 2] = 5
-    # perspective_transform[1, 2] = 15
+    perspective_transform = np.eye(3, dtype=np.float64) - 0.001 * np.random.randn(3, 3)
+    perspective_transform[0, 2] = 5
+    perspective_transform[1, 2] = 5
 
     affine_transform = [70, -5, -15, 0.8, 0.8]
     rigid_transform = [1, 5, -6]
@@ -39,25 +39,21 @@ def main(params):
                              [5, -5, 5, 0.9, 0.90],
                              [5, 10, 10, 0.95, 0.95]]
     perspective_transform_list = [
-                                  [[9.97962760e-01,  1.87656307e-03, -5.00000000e+00],
-                                   [-1.77597560e-04,  1.00062371e+00, -5.00000000e+00],
-                                   [-4.64119328e-04, -8.44921859e-05,  1.00048039e+00]],
+        [[9.98973341e-01, - 7.31692941e-05,  9.37318374e-04],
+         [-2.23558816e-03,  1.00117543e+00, - 3.75353094e-04],
+         [8.88458505e-04,  3.31434113e-04,  1.00444452e+00]],
 
-                                  [[9.99622225e-01, - 1.45607355e-03, - 5.00000000e+00],
-                                   [8.72527131e-04,  9.99464513e-01,  5.00000000e+00],
-                                   [-1.43820521e-03,  1.38813994e-03,  1.00001295e+00]],
+        [[9.99975115e-01, - 3.13277331e-03,  2.01608106e-03],
+         [-1.78004689e-03,  9.99731592e-01,  1.75828657e-03],
+         [-1.67729496e-04,  5.81974154e-04,  1.00318653e+00]],
 
-                                  # [[1.00022373e+00,  1.79532565e-03,  5.00000000e+00],
-                                  #  [1.49052863e-03,  1.00074024e+00, -5.00000000e+00],
-                                  #  [2.18009167e-03, - 1.62779992e-03,  1.00009343e+00]],
+        [[1.00027271e+00, - 1.49952337e-04,  1.58446339e-03],
+         [9.65119604e-04,  9.99892158e-01,  5.00000000e+00],
+         [-4.65758781e-04, - 2.84519347e-04,  1.00073029e+00]],
 
-                                  [[1.00047720e+00, - 1.92540739e-04,  5.00000000e+00],
-                                   [2.17379389e-03,  1.00074264e+00,  5.00000000e+00],
-                                   [2.75700615e-04,  1.97223155e-04,  1.00077639e+00]],
-
-                                  [[9.99974479e-01, - 8.74546788e-04,  5.00000000e+00],
-                                   [-1.11169385e-03,  9.99651386e-01,  5.00000000e+00],
-                                   [2.95366985e-04,  4.35728917e-04,  9.99527605e-01]]
+        [[9.99458100e-01,  2.03181632e-03,  5.00000000e+00],
+         [-1.64182089e-04,  9.99429672e-01,  5.00000000e+00],
+         [-4.30677510e-04, - 2.53577463e-04,  9.99401458e-01]]
     ]
 
     # perspective_transform_list = [perspective_transform]
@@ -70,9 +66,10 @@ def main(params):
 
     transform_list = perspective_transform_list
     result_params_list = []
+    true_transform_params_list = []
 
     moving_image_list = []
-    combined = np.zeros((fixed_image.shape[0], fixed_image.shape[1], len(transform_list)))
+    registered_images_list = np.zeros((fixed_image.shape[0], fixed_image.shape[1], len(transform_list)))
 
     for i in range(len(transform_list)):
 
@@ -81,25 +78,27 @@ def main(params):
 
         result_params_list.append(optimise(optimiser, initial_transform, fixed_image, moving_image_list[i],
                                            similarity_metric, params.patch_size))
-
-        # fig, (ax0, ax1) = plt.subplots(1, 2, figsize=(10, 10))
-        # ax0.imshow(moving_image_list[i] - fixed_image, cmap='gray')
-        # ax0.set_title("before reg")
-        # ax1.imshow(transform_image(moving_image, result_params_list[i]) - fixed_image, cmap='gray')
-        # ax1.set_title("after reg")
-        # plt.show()
-        # return
+        true_transform_params_list.append(np.linalg.inv(np.reshape(transform_list[i], (3, 3))))
 
         # Transform the moving images with the found parameters
-        combined[:, :, i] = transform_image(moving_image, result_params_list[i])
+        registered_images_list[:, :, i] = transform_image(moving_image_list[i], result_params_list[i])
 
+        fig, (ax0, ax1, ax2) = plt.subplots(1, 3, figsize=(10, 10))
+        ax0.imshow(moving_image_list[i] - fixed_image, cmap='gray')
+        ax0.set_title("before reg")
+        ax1.imshow(registered_images_list[:, :, i] - fixed_image, cmap='gray')
+        ax1.set_title("calculated reg")
+        ax2.imshow(transform_image(moving_image_list[i], true_transform_params_list[i]) - fixed_image, cmap="gray")
+        ax2.set_title("true reg")
+        plt.show()
+        # return
 
     # calculate variance of the combined results
-    combined_variance = np.var(combined, axis=2)
+    combined_variance = np.var(registered_images_list, axis=2)
     combined_variance = combined_variance / np.max(combined_variance)
 
     # create an overlay of one of the registered images (in this case it's the first one) with the variance map
-    moving_overlayed_with_var = create_two_image_overlay(combined[:, :, 0], combined_variance, alpha=0.6, cmap="plasma")
+    moving_overlayed_with_var = create_two_image_overlay(registered_images_list[:, :, 0], combined_variance, alpha=0.6, cmap="plasma")
 
     # Display the output
     fig = plt.figure(figsize=(12, 6))
@@ -146,3 +145,5 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     main(args)
+
+
