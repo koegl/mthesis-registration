11a2dfea40537d4b78ff909933c34b4e052dfc81
Fryderyk Kögl <koeglfryderyk@gmail.com>
2022-05-16 14:38:13 -0400

    generalised patch extraction to 3D

--- utils.py
+++ utils.py
@@ -11,7 +11,6 @@ def save_np_array_as_nifti(array, path):
     Save an nd array as a nifti file.
     :param array: the nd array to save
     :param path: the path to save the nifti file
-    :return:
     """
 
     img = nib.Nifti1Image(array, np.eye(4))
@@ -34,45 +33,53 @@ def create_radial_gradient(width, height, depth):
     return r
 
 
-def extract_square_patch_offset(image, center, size, offset=None):
+def extract_cubical_patch_offset(image, center, size, offset=None):
     """
-    Extract a square patch from the image.
-    :param image: the image as an nd array
-    :param center: the center of the square patch
-    :param size: the size of the square patch
-    :param offset: the offset of the square patch
-    :return: the square patch as an nd array
+    Extract a cubical patch from the image.
+    :param image: the volume as an nd array
+    :param center: the center of the cubical patch
+    :param size: the size of the cubical patch
+    :param offset: the offset of the cubical patch
+    :return: the cubical patch as an nd array
     """
 
     if offset is None:
-        offset = [0, 0]
+        offset = [0, 0, 0]
+
+    assert len(offset) == 3, "Offset must be a 3D vector"
+    assert len(center) == 3, "Center must be a 3D vector"
+    assert isinstance(size, int), "Size must be a scalar integer"
 
     x_min = center[0] - size // 2 + offset[0]
     x_max = center[0] + size // 2 + offset[0]
     y_min = center[1] - size // 2 + offset[1]
     y_max = center[1] + size // 2 + offset[1]
+    z_min = center[2] - size // 2 + offset[2]
+    z_max = center[2] + size // 2 + offset[2]
 
     # check if the patch is out of bounds
-    if x_min < 0 or x_max >= image.shape[0] or y_min < 0 or y_max >= image.shape[1]:
+    if x_min < 0 or x_max >= image.shape[0] or \
+       y_min < 0 or y_max >= image.shape[1] or \
+       z_min < 0 or z_max >= image.shape[2]:
         warnings.warn("The patch is out of bounds.")
         return np.zeros((1, 1))
 
-    return image[x_min:x_max, y_min:y_max]
+    return image[x_min:x_max, y_min:y_max, z_min:z_max]
 
 
 def extract_overlapping_patches(image_fixed, image_moving, centre, size, offset=None):
     """
-    Extract overlapping patches from the two images. One of the image patches will be offset by 'offset'
-    :param image_fixed: the image with the standard patch
-    :param image_moving: the image with the offset patch
+    Extract overlapping patches from the two volumes. One of the volume patches will be offset by 'offset'
+    :param image_fixed: the volume with the standard patch
+    :param image_moving: the volume with the offset patch
     :param centre: centre of the patch
     :param size: size of the patch
     :param offset: offset of the image_moving patch
     :return:
     """
 
-    patch_fixed = extract_square_patch_offset(image_fixed, centre, size, offset=None)
+    patch_fixed = extract_cubical_patch_offset(image_fixed, centre, size, offset=None)
 
-    patch_moving = extract_square_patch_offset(image_moving, centre, size, offset=offset)
+    patch_moving = extract_cubical_patch_offset(image_moving, centre, size, offset=offset)
 
     return patch_fixed, patch_moving
