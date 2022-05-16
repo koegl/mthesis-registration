4e55635701097107c349a8a6df4589740fc88534
Fryderyk Kögl <koeglfryderyk@gmail.com>
2022-05-16 13:58:17 -0400

    added a funciton to create a radial gradient
    can be used to extarct patches

---
+++ utils.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+def create_radial_gradient(width, height):
+    """
+    Create a radial gradient.
+    :param width: width of the image
+    :param height: height of the image
+    :return: the gradient image as an nd array
+    """
+
+    x, y = np.meshgrid(np.linspace(0, 1, width), np.linspace(0, 1, height))
+    r = np.sqrt(x ** 2 + y ** 2)
+
+    return r
