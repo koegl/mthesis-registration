8c8432d2c174445587f8f1ff41c8ad9d8fc1688c
Fryderyk Kögl <koeglfryderyk@gmail.com>
2022-05-16 14:08:09 -0400

    added function to extract a square patch from a 2D image

--- utils.py
+++ utils.py
@@ -13,3 +13,20 @@ def create_radial_gradient(width, height):
     r = np.sqrt(x ** 2 + y ** 2)
 
     return r
+
+
+def extract_square_patch(image, center, size):
+    """
+    Extract a square patch from the image.
+    :param image: the image as an nd array
+    :param center: the center of the square patch
+    :param size: the size of the square patch
+    :return: the square patch as an nd array
+    """
+
+    x_min = center[0] - size // 2
+    x_max = center[0] + size // 2
+    y_min = center[1] - size // 2
+    y_max = center[1] + size // 2
+
+    return image[x_min:x_max, y_min:y_max]
