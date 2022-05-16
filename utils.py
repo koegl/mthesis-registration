a776cc52b3d2606b909007cc13bce0e796316160
Fryderyk Kögl <koeglfryderyk@gmail.com>
2022-05-16 14:14:50 -0400

    added an optional offset for the patch extractor

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import numpy as np
 import warnings
 
+
 def create_radial_gradient(width, height):
     """
     Create a radial gradient.
@@ -15,19 +16,23 @@ def create_radial_gradient(width, height):
     return r
 
 
-def extract_square_patch(image, center, size):
+def extract_square_patch_offset(image, center, size, offset=None):
     """
     Extract a square patch from the image.
     :param image: the image as an nd array
     :param center: the center of the square patch
     :param size: the size of the square patch
+    :param offset: the offset of the square patch
     :return: the square patch as an nd array
     """
 
-    x_min = center[0] - size // 2
-    x_max = center[0] + size // 2
-    y_min = center[1] - size // 2
-    y_max = center[1] + size // 2
+    if offset is None:
+        offset = [0, 0]
+
+    x_min = center[0] - size // 2 + offset[0]
+    x_max = center[0] + size // 2 + offset[0]
+    y_min = center[1] - size // 2 + offset[1]
+    y_max = center[1] + size // 2 + offset[1]
 
     # check if the patch is out of bounds
     if x_min < 0 or x_max >= image.shape[0] or y_min < 0 or y_max >= image.shape[1]:
