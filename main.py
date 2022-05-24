ba36e2fee1d0dc7540131b804c8fe6a1702d7b9c
Fryderyk Kögl <koeglfryderyk@gmail.com>
2022-05-24 18:53:18 -0400

    clarified params in main

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import glob
 import tensorflow as tf
 
 from logic.cnn_classifier import Classifier
-from utils import get_datasets, convert_cmd_args_to_correct_type, restricted_float
+from utils import get_datasets, convert_cmd_args_to_correct_type
 from train import train
 
 
@@ -32,18 +32,18 @@ def main(params):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("-tvd", "--train_and_val_dir", default="/Users/fryderykkogl/Data/ViT_training_data/data_overfit/train",
+    parser.add_argument("-tvd", "--train_and_val_dir", default="/Users/fryderykkogl/Data/ViT_training_data/renamed_data/train",
                         help="path to the training data")
-    parser.add_argument("-td", "--test_dir", default="/Users/fryderykkogl/Data/ViT_training_data/data_overfit/test",
+    parser.add_argument("-td", "--test_dir", default="//Users/fryderykkogl/Data/ViT_training_data/renamed_data/test",
                         help="path to the test data")
 
-    parser.add_argument("-bs", "--batch_size", default=2)
-    parser.add_argument("-e", "--epochs", default=4)
+    parser.add_argument("-bs", "--batch_size", default=1)
+    parser.add_argument("-e", "--epochs", default=10)
 
     parser.add_argument("-lr", "--learning_rate", default=0.001)
 
-    parser.add_argument("-off", "--over_fit_images", default=2, type=restricted_float,
-                        help="if overfit is 1, the full train set will be used, if 0 then none and then everything in between")
+    parser.add_argument("-off", "--over_fit_images", default=50, type=int,
+                        help="amount of images to be used for over-fit-training")
 
     parser.add_argument("-v", "--validate", default=True, type=bool,
                         help="if true, the model will be validated")
