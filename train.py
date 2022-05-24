13ed7e23bed8a50bb903b3efcaf0a6b20ce2b919
Fryderyk Kögl <koeglfryderyk@gmail.com>
2022-05-24 12:39:08 -0400

    added a separate file for training functions

---
+++ train.py
@@ -0,0 +1,59 @@
+import tensorflow as tf
+
+
+@tf.function
+def train_step(images, labels, model, loss_function, optimiser, train_loss, train_accuracy):
+    with tf.GradientTape() as tape:
+        # training=True is only needed if there are layers with different
+        # behavior during training versus inference (e.g. Dropout).
+
+        predictions = model(images, training=True)
+        loss = loss_function(labels, predictions)
+
+    gradients = tape.gradient(loss, model.trainable_variables)
+    optimiser.apply_gradients(zip(gradients, model.trainable_variables))
+
+    train_loss(loss)
+    train_accuracy(labels, predictions)
+
+
+@tf.function
+def test_step(images, labels, model, loss_function, test_loss, test_accuracy):
+    # training=False is only needed if there are layers with different
+    # behavior during training versus inference (e.g. Dropout).
+    predictions = model(images, training=False)
+    t_loss = loss_function(labels, predictions)
+
+    test_loss(t_loss)
+    test_accuracy(labels, predictions)
+
+
+@tf.function
+def train(model, optimiser, loss_function, train_ds, test_ds, epochs):
+    # metrics for measuring loss and accuracy
+    train_loss = tf.keras.metrics.Mean(name='train_loss')
+    train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='train_accuracy')
+
+    test_loss = tf.keras.metrics.Mean(name='test_loss')
+    test_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='test_accuracy')
+
+    for epoch in range(epochs):
+        # reset the metrics at the start of the next epoch
+        train_loss.reset_states()
+        train_accuracy.reset_states()
+        test_loss.reset_states()
+        test_accuracy.reset_states()
+
+        for images, labels in train_ds:
+            train_step(images, labels, model, loss_function, optimiser, train_loss, train_accuracy)
+
+        for test_images, test_labels in test_ds:
+            test_step(test_images, test_labels, model, loss_function, test_loss, test_accuracy)
+
+        print(
+            f'Epoch {epoch + 1}, '
+            f'Loss: {train_loss.result()}, '
+            f'Accuracy: {train_accuracy.result() * 100}, '
+            f'Test Loss: {test_loss.result()}, '
+            f'Test Accuracy: {test_accuracy.result() * 100}'
+        )
