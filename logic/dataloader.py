dcfbd812e70c77bdcfa43c2774d0e82b54d8f9e6
Fryderyk Kögl <koeglfryderyk@gmail.com>
2022-06-02 16:35:07 -0400

    added val loader and train val split and dataset reduction to dataloader

--- logic/dataloader.py
+++ logic/dataloader.py
@@ -1,4 +1,3 @@
-import nibabel as nib
 import glob
 import os
 import numpy as np
@@ -6,8 +5,7 @@ import numpy as np
 import torch
 from torchvision import transforms
 from torch.utils.data import Dataset, DataLoader
-
-from logic.patcher import Patcher
+from sklearn.model_selection import train_test_split
 
 
 class PatchDataset(Dataset):
@@ -56,15 +54,37 @@ def get_transforms():
     return train_transforms, val_transforms, test_transforms
 
 
-def get_data_loader(data_path="/Users/fryderykkogl/Data/temp/data_npy", batch_size=1):
+def get_data_loaders(params):
+
+    data_path = params.train_and_val_dir
+    batch_size = params.batch_size
+
+    # get paths to the patches and labels
+    patch_file_path_list = glob.glob(os.path.join(data_path, "*_fixed_and_moving.npy"))
+    patch_file_path_list.sort()
+
+    # reduce the dataset size if necessary
+    if params.dataset_size >= len(patch_file_path_list):
+        params.dataset_size = len(patch_file_path_list)
+
+    patch_file_path_list = patch_file_path_list[0:params.dataset_size]
+    label_path = os.path.join(data_path, "labels.npy")
+
+    # train and val split
+    if params.validate is True:
+        train_list, val_list = train_test_split(patch_file_path_list, test_size=0.2, random_state=params.seed)
+    else:
+        train_list, val_list = patch_file_path_list, patch_file_path_list
 
     # get transforms
-    train_transforms, _, _ = get_transforms()
+    train_transforms, val_transforms, _ = get_transforms()
 
-    # create the dataset
-    train_dataset = PatchDataset(data_path, train_transforms)
+    # create the datasets
+    train_dataset = PatchDataset(train_list, label_path, train_transforms)
+    val_dataset = PatchDataset(val_list, label_path, val_transforms)
 
     # pass the dataset to the dataloader
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True)
 
-    return train_loader
+    return train_loader, val_loader, None
