e7a13fc422faf3b5fad0f645020916ecaaece6ed
Fryderyk Kögl <koeglfryderyk@gmail.com>
2022-06-02 16:39:58 -0400

    added main for training

--- main.py
+++ main.py
@@ -6,3 +6,60 @@
 # 3. create and save the patches as numpy arrays in a new folder
 # 3. pass this big list of lists to the dataloader
 
+# https://sebastianraschka.com/blog/2022/pytorch-m1-gpu.html
+
+import argparse
+
+import torch.optim as optim
+import torch.nn as nn
+
+from utils import seed_everything, initialise_wandb, get_architecture
+from logic.train import train
+from logic.dataloader import get_data_loaders
+
+
+def main(params):
+
+    # set seed
+    seed_everything(params.seed)
+
+    # get train, val, and test data loaders
+    train_loader, val_loader, test_loader = get_data_loaders(params)
+
+    # get the model
+    model = get_architecture()
+
+    # set-up loss-function
+    criterion = nn.CrossEntropyLoss()
+
+    # set up optimizer
+    optimizer = optim.Adam(model.parameters(), lr=params.learning_rate)
+
+    # logging
+    if params.loggin == "wandb":
+        # set up logging with wandb
+        initialise_wandb(params, len(train_loader.dataset), len(val_loader.dataset),
+                         project="Classification", entity="fryderykkogl")
+
+    # train the model
+    train(params, train_loader, model, criterion, optimizer, val_loader, params["device"])
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-bs", "--batch_size", default=2)
+    parser.add_argument("-e", "--epochs", default=7)
+    parser.add_argument("-lr", "--learning_rate", default=0.0001)
+    parser.add_argument("-s", "--seed", default=42, help="For seeding eveyrthing")
+    parser.add_argument("-tvd", "--train_and_val_dir", default="/Users/fryderykkogl/Data/ViT_training_data/data_overfit/train",
+                        help="Directory of the training data (and validation")
+    parser.add_argument("-dv", "--device", default="cpu", choices=["cpu", "mps"])
+    parser.add_argument("-ds", "--dataset_size", default=4, type=int, help="Amount of images used for training")
+    parser.add_argument("-v", "--validate", default=False, type=bool, help="Choose whether to validate or not")
+    parser.add_argument("-lg", "--logging", default="print", choices=["print", "wandb"])
+
+    args = parser.parse_args()
+
+    main(args)
+
