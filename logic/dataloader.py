403bfe582dc6abf2902cdf545711d2160673d618
Fryderyk Kögl <koeglfryderyk@gmail.com>
2022-06-02 11:59:00 -0400

    added patch dataloader

---
+++ logic/dataloader.py
@@ -0,0 +1,22 @@
+from PIL import Image
+from torch.utils.data import Dataset
+import torch
+
+
+class PatchDataset(Dataset):
+    def __init__(self, file_list, transform=None):
+        self.file_list = file_list
+        self.transform = transform
+
+    def __len__(self):
+        self.filelength = len(self.file_list)
+        return self.filelength
+
+    def __getitem__(self, idx):
+        img_path = self.file_list[idx]
+        img = Image.open(img_path)
+        img_transformed = self.transform(img)
+
+        label = img_path.split("/")[-1].split(".")[0]
+        label = torch.tensor([1.0, 0.0]) if "dog" in label else torch.tensor([0.0, 1.0])
+        return img_transformed, label
