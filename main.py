63dfc21d2603b1956f911bbfa58850c4fb8b2047
Fryderyk Kögl <koeglfryderyk@gmail.com>
2022-06-02 16:08:06 -0400

    added plan to main

---
+++ main.py
@@ -0,0 +1,8 @@
+
+
+# PLAN
+# 1. get a list of all files (.niftis)
+# 2. for each file create a list of centres - combine into list of lists
+# 3. create and save the patches as numpy arrays in a new folder
+# 3. pass this big list of lists to the dataloader
+
