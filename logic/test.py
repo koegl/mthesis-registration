4ce69ff93682cb820791f3dddb711d252ad0c5d0
Fryderyk Kögl <koeglfryderyk@gmail.com>
2022-06-04 13:41:46 -0400

    updated test to also create and save cisualisations

--- logic/test.py
+++ logic/test.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import tqdm
 from tqdm import trange
@@ -9,34 +10,40 @@ from utils import calculate_accuracy, display_volume_slice, get_label_id_from_la
 from dataloader import get_test_loader
 from architectures.densenet3d import DenseNet
 from logic.patcher import Patcher
+from visualisations import visualise_per_class_accuracies
+from logic.patcher import Patcher
 
 
 def test(model, test_loader):
     patcher = Patcher("", "", "", 10, False)
     x = patcher.label_to_offset_dict
 
+    length_of_test_loader = len(test_loader)
+
     test_accuracy = 0.0
 
     acc_list = np.zeros(20)
+    class_amount = np.zeros(20)
 
     with torch.no_grad():
 
         t = trange(len(test_loader), desc=f"Testing patches ({test_accuracy:.2f})", leave=True)
         test_loader = iter(test_loader)
-        for i in t:
+        for _ in t:
             data, label = next(test_loader)
             data = data.to(torch.float32)
             label = label.to(torch.float32)
 
             label_np = label.squeeze().detach().cpu().numpy()
             idx = label_np.argmax()
+            class_amount[idx] += 1
             label_id = get_label_id_from_label(label_np)
             offset = x[label_id]
             # display_volume_slice(data.squeeze().detach().cpu().numpy(), offset)
 
             test_output = model(data)
             acc = calculate_accuracy(test_output, label)
-            test_accuracy += acc / len(test_loader)
+            test_accuracy += acc / length_of_test_loader
 
             if acc >= 1.0:
                 acc_list[idx] += 1
@@ -44,18 +51,23 @@ def test(model, test_loader):
             t.set_description(f"Testing patches ({test_accuracy:.2f})")
             t.refresh()
 
-    print(f"Test accuracy: {test_accuracy:.4f}")
-    print(f"Accuracy per label: {acc_list}")
+    return test_accuracy, acc_list, class_amount
 
 
 # load model
 model = DenseNet()
-model_params = torch.load("/Users/fryderykkogl/Data/models/robust-oath-133/model_epoch23_valacc0.954.pt",
+model_params = torch.load("/Users/fryderykkogl/Dropbox (Partners HealthCare)/Experiments/dense net classification 1/robust-oath-133/model_epoch23_valacc0.954.pt",
                           map_location=torch.device('cpu'))
 model.load_state_dict(model_params['model_state_dict'])
 model.eval()
 
 # load test data
-loader = get_test_loader("/Users/fryderykkogl/Data/patches/test_data/20211129_craini_golby/reslcied_perfect_true")
+loader = get_test_loader("/Users/fryderykkogl/Dropbox (Partners HealthCare)/Experiments/dense net classification 1/20211129_craini_golby/resliced_perfect_true")
 
-test(model, loader)
+test_accuracy, acc_list, class_amount = test(model, loader)
+
+# visualise
+patcher = Patcher("", "", "", 10, False)
+x = patcher.offsets
+visualise_per_class_accuracies(acc_list/class_amount, x, f"Test per-class accuracies for {len(loader)} 'perfect' patches")
+plt.savefig("/Users/fryderykkogl/Dropbox (Partners HealthCare)/Experiments" + "/test_per_class_accuracies_true.png", dpi=250)
