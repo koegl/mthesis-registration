81c9ec68daeb3167fd095e6cf9abd056b9b47816
Fryderyk Kögl <koeglfryderyk@gmail.com>
2022-06-03 22:11:58 -0400

    added better model saving

--- logic/train.py
+++ logic/train.py
@@ -36,6 +36,8 @@ def train_step(train_loader, device, model, criterion, optimizer, epoch, logging
         print(f"\n{epoch}:\tTime: {perf_counter() - now:.2f}s;\t Train loss: {epoch_loss:.2f};\tTrain accuracy: {epoch_accuracy:.2f}",
               end='\t')
 
+    return epoch_loss
+
 
 def val_step(val_loader, device, model, criterion, epoch, logging):
 
@@ -61,24 +63,31 @@ def val_step(val_loader, device, model, criterion, epoch, logging):
     else:
         print(f"Val loss: {epoch_val_loss:.2f};\tVal accuracy: {epoch_val_accuracy:.2f}")
 
+    return epoch_val_loss
+
 
 def train(params, train_loader, model, criterion, optimizer, val_loader, device, save_path="models/model.pt"):
     """
     Train the model for a given number of epochs and save the model at the end of training.
     """
+    val_loss = 0
 
     for epoch in range(params.epochs):
 
         # train step
-        train_step(train_loader, device, model, criterion, optimizer, epoch, params.logging)
+        train_loss = train_step(train_loader, device, model, criterion, optimizer, epoch, params.logging)
 
         # eval step
         if params.validate is True:
-            val_step(val_loader, device, model, criterion, epoch, params.logging)
+            val_loss = val_step(val_loader, device, model, criterion, epoch, params.logging)
 
         # save model
-        # split_save_path = save_path.split('.')
-        # new_save_path = split_save_path[0] + '_' + str(epoch) + '.' + split_save_path[1]
+        torch.save({"epoch": epoch,
+                    "model_state_dict": model.state_dict(),
+                    "optimizer_state_dict": optimizer.state_dict(),
+                    "loss": train_loss},
+                   f"/Users/fryderykkogl/Documents/university/master/thesis/code.nosync/mthesis-registration/models/model_epoch{epoch}_loss{train_loss}.pt"
+                   )
 
     torch.save(model, save_path)
 
