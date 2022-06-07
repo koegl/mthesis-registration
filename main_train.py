14b32d0a5aac02efaefe4276e4bad5cd2f3dba7d
Fryderyk Kögl <koeglfryderyk@gmail.com>
2022-06-06 20:16:02 -0400

    updated default main params in train

--- main_train.py
+++ main_train.py
@@ -57,17 +57,17 @@ def main(params):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("-bs", "--batch_size", default=10)
-    parser.add_argument("-e", "--epochs", default=100)
+    parser.add_argument("-bs", "--batch_size", default=32)
+    parser.add_argument("-e", "--epochs", default=400)
     parser.add_argument("-lr", "--learning_rate", default=0.001)
     parser.add_argument("-s", "--seed", default=42, help="For seeding eveyrthing")
-    parser.add_argument("-tvd", "--train_and_val_dir", default="/Users/fryderykkogl/Data/temp/data_npy",
+    parser.add_argument("-tvd", "--train_and_val_dir", default="/Users/fryderykkogl/Data/patches/data_npy",
                         help="Directory of the training data (and validation")
     parser.add_argument("-dv", "--device", default="cpu", choices=["cpu", "mps"])
     parser.add_argument("-ds", "--dataset_size", default=400, type=int, help="Amount of images used for training")
     parser.add_argument("-v", "--validate", default=True, type=bool, help="Choose whether to validate or not")
     parser.add_argument("-lg", "--logging", default="wandb", choices=["print", "wandb"])
-    parser.add_argument("-at", "--architecture_type", default="vit", choices=["densenet", "vit"])
+    parser.add_argument("-at", "--architecture_type", default="densenet", choices=["densenet", "vit"])
 
     args = parser.parse_args()
 
