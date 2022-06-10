b7ed749ececacf694986251811f42875c116447b
Fryderyk Kögl <koeglfryderyk@gmail.com>
2022-06-10 02:31:32 -0400

    parallelized IR saving

--- impulseresponse.py
+++ impulseresponse.py
@@ -1,64 +1,90 @@
-import nibabel as nib
 import numpy as np
 import torch
-from ast import literal_eval
+import glob
+import os
+from tqdm import tqdm
+import shutil
+import matplotlib.pyplot as plt
+from offsets import offsets_xyz as offsets
+from joblib import Parallel, delayed
+import multiprocessing
 
 from logic.patcher import Patcher
-from visualisations import display_volume_slice
-from utils import patch_inference
+from visualisations import display_volume_slice, visualise_per_class_accuracies
+from utils import patch_inference, get_label_from_label_id
 from architectures.densenet3d import DenseNet
 from architectures.vit_standard_3d import ViTStandard3D
+from time import perf_counter, sleep
 
 
-def ssd(a, b):
-    dif = a.ravel() - b.ravel()
-    return np.dot(dif, dif)
+def create_irs(new_offset, i, model):
+    # delete directory if it exists
+    if os.path.exists(f"/Users/fryderykkogl/Data/patches/val_npy_{i}"):
+        shutil.rmtree(f"/Users/fryderykkogl/Data/patches/val_npy_{i}")
+    os.mkdir(f"/Users/fryderykkogl/Data/patches/val_npy_{i}")
 
+    # generate patches
+    patcher = Patcher(
+        load_directory=f"/Users/fryderykkogl/Data/patches/val_nii",
+        save_directory=f"/Users/fryderykkogl/Data/patches/val_npy_{i}",
+        file_type="nii.gz",
+        centres_per_dimension=12,
+        perfect_truth=False,
+        patch_size=32,
+        scale_dist=1.5,
+        offset_multiplier=1,
+        rescale=True,
+        save_type="float16",
+        offsets=[new_offset]
+    )
+    original_patcher = Patcher("", "", "", 10, "")
+    # create new patches
+    patcher.create_and_save_all_patches_and_labels()
 
-# Load the volume
-# volume = nib.load("/Users/fryderykkogl/Data/patches/data_nii/0.nii.gz").get_fdata()
-# np.save("/Users/fryderykkogl/Data/patches/numpy_volume", volume)
-volume = np.load("/Users/fryderykkogl/Data/patches/numpy_volume.npy")
+    # get a list of patches
+    patches = glob.glob(f"/Users/fryderykkogl/Data/patches/val_npy_{i}/*.npy")
+    patches.sort()
 
-# get a patch pair
-patcher = Patcher("", "", "", 10, False)
+    ir = np.zeros(20)
 
-results = []
+    for patch_path in tqdm(patches):
+        patch = np.load(patch_path)
 
-true_offsets = [[0, 0, 0], [16, 4, -8], [4, 4, 4], [4, 0, 4], [0, 4, 4], [-4, -4, 0], [-4, 0, -4], [0, -4, -4]]
-center = [350, 100, 300]
+        # predict displacement
+        _, output, _ = patch_inference(model, patch, original_patcher.offsets)
+
+        ir += output
+
+    if len(patches) == 0:
+        return
+
+    ir /= len(patches)
+
+    visualise_per_class_accuracies(ir, [np.array2string(x, separator=',') for x in original_patcher.offsets],
+                                   f"Average IR for {len(patches)} patches with offset f{new_offset}",
+                                   lim=(-15, 15))
+    plt.savefig(
+        "/Users/fryderykkogl/Dropbox (Partners HealthCare)/DL/Experiments/mr impulse response/08-dense-267k-mr-balmy-sweep-3" +
+        f"/ir_{new_offset}.png",
+        dpi=250)
+    plt.close()
+
+    if os.path.exists(f"/Users/fryderykkogl/Data/patches/val_npy_{i}"):
+        shutil.rmtree(f"/Users/fryderykkogl/Data/patches/val_npy_{i}")
+
 
 # get the model
-# model = ViTStandard3D(dim=128, volume_size=32, patch_size=4, num_classes=20, channels=2, depth=6, heads=8, mlp_dim=2048,
-#                       dropout=0.1, emb_dropout=0.1, device="cpu")
-model = DenseNet(num_init_features=10)
-model_dict = torch.load("/Users/fryderykkogl/Desktop/model_epoch14_valacc0.678.pt",
-                        map_location=torch.device('cpu'))
-model.load_state_dict(model_dict['model_state_dict'])
-model.eval()
-acc = 0
+model_inference = DenseNet(num_init_features=10)
+model_dict = torch.load(
+    "/Users/fryderykkogl/Dropbox (Partners HealthCare)/DL/Models/08-dense-267k-mr-balmy-sweep-3/model_epoch14_valacc0.678.pt",
+    map_location=torch.device('cpu'))
+model_inference.load_state_dict(model_dict['model_state_dict'])
+model_inference.eval()
 
-for i in range(len(true_offsets)):
-    true_offset = true_offsets[i]
-    packet = patcher.extract_overlapping_patches(volume, volume, center, offset=true_offset)
+num_cores = multiprocessing.cpu_count()
+return_list = Parallel(n_jobs=num_cores)(delayed(create_irs)(offsets[i], i, model_inference)
+                                         for i in range(len(offsets)))
 
-    patches = np.stack((packet[0], packet[1]), 0)
-
-    # get offsets
-    offsets = patcher.offsets.astype(np.float32)
-    offsets[0] = np.asarray([0.0, 0.0, 0.0])
-
-    # predict displacement
-    print(true_offset)
-    e_d = patch_inference(model, patches, offsets)
-
-    acc += ssd(e_d, np.asarray(true_offset)) / len(true_offsets)
-
-    # print results
-    np.set_printoptions(precision=2)
-    print(f"True displacement:\t\t{true_offset}\n"
-          f"Predicted displacement:\t{e_d}\n")
-    texx = 2
-    # _ = display_volume_slice(patches, f"True displacement:         {true_offset}\n"
-    #                                  f"Predicted displacement: {e_d}")
-
+print("="*30)
+print("Done")
+print("="*30)
