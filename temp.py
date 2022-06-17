0fb3635d87a21bd6feb760a836d8e91bc5d2262c
Fryderyk Kögl <koeglfryderyk@gmail.com>
2022-06-16 21:33:03 -0400

    script to test offset plot function

--- temp.py
+++ temp.py
@@ -5,12 +5,12 @@ import matplotlib.patches as patches
 from helpers.visualisations import plot_offsets
 
 
-offset = [16, 0, 2]
+offset = [4, -8, 0]
 predicted_offsets = [
     ([4, 0, 0], 0.3),
-    ([8, 0, 0], 0.5),
+    ([0, 16, 0], 0.5),
     ([16, 0, 0], 0.2),
-    ([0, 4, 7], 0.3),
+    ([0, 4, 0], 0.3),
 ]
 
-plot_offsets(offset, predicted_offsets)
+plot_offsets(offset, [3, -10, 0], predicted_offsets)
