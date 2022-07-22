31616d72d97ad1b980bb4d751b6ea5bda8366ed3
Fryderyk Kögl <koeglfryderyk@gmail.com>
2022-07-22 15:31:38 -0400

    added example with map coordinates

--- temp.py
+++ temp.py
@@ -1,113 +1,74 @@
+import scipy.ndimage as ndimage
+
+
+import argparse
 import numpy as np
-from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from time import perf_counter
+import scipy.ndimage as ndimage
+import nibabel as nib
 
+import logic.patcher
+from logic.patcher import Patcher
+import helpers.visualisations as visualisations
 import helpers.utils as utils
+import helpers.volumes as volumes
 
 
-primary_n = np.array([[40., 20., 20.],
-                    [20., 40., 20.],
-                    [20., 20., 40.],
-                    [20., 20., 20.],
-                    [30., 20., 20.]
-                    ])
-secondary_n = np.array([[65., 10., 30.],  # (20, -10, 10)
-                      [40., 30., 30.],
-                      [40., 10., 50.],
-                      [40., 10., 30.],
-                      [50., 10., 30.]
-                      ])
+def calculate_resulting_vector(variance_list, ed_list, mode="inverse"):
+    assert mode in ["inverse", "oneminus"], "mode must be either 'inverse' or 'oneminus'"
+    assert len(variance_list) == len(ed_list), "variance and ed lists must have the same length"
+    assert isinstance(variance_list, list), "variance list must be a list"
+    assert isinstance(ed_list, list), "ed list must be a list"
+
+    variance_list = np.array(variance_list)
+    ed_list = np.array(ed_list)
+
+    if mode == "inverse":
+        variance = 1 / variance_list
+        resulting_vector = np.dot(variance, ed_list) / np.sum(variance)
+    else:
+        variance_list = variance_list / np.max(variance_list)
+        variance_list = 1 - variance_list
+        resulting_vector = np.dot(variance_list, ed_list) / np.sum(variance_list)
+
+    return resulting_vector
 
 
+def main(params):
+    volume = np.load(params.offset_volume_path).astype(np.float32)
+    transform = volumes.create_transform_matrix(0, 0, 0, 7, 7, 7)
 
-primary = np.array([[40., 20., 20., 1],
-                    [20., 40., 20., 1],
-                    [20., 20., 40., 1],
-                    [20., 20., 20., 1],
-                    [30., 20., 20., 1]
-                    ])
+    t = perf_counter()
+    volume_normal_affine = ndimage.affine_transform(volume, transform)
+    t_normal_affine = perf_counter() - t
 
-secondary = np.array([[65., 10., 30., 1],  # (20, -10, 10)
-                      [40., 30., 30., 1],
-                      [40., 10., 50., 1],
-                      [40., 10., 30., 1],
-                      [50., 10., 30., 1]
-                      ])
+    t = perf_counter()
+    i, j, k = volume.shape
+    i_vals, j_vals, k_vals = np.meshgrid(range(j), range(j), range(k), indexing='ij')
+    coords = np.array([i_vals, j_vals, k_vals]).transpose((1, 2, 3, 0))
 
-np.set_printoptions(suppress=True)
-np.set_printoptions(formatter={'float': '{: 0.0f}'.format})
+    new_coords = nib.affines.apply_affine(transform, coords)
+    new_coords = new_coords.transpose(3, 0, 1, 2)
+    volume_map_coordinates = ndimage.map_coordinates(volume, new_coords)
+    t_map_coordinates = perf_counter() - t
 
+    print(5)
 
-def least_squares_transform():
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
 
-    # Pad the data with ones, so that our transformation can do translations too
-    n = primary.shape[0]
-    pad = lambda x: np.hstack([x, np.ones((x.shape[0], 1))])
-    unpad = lambda x: x[:,:-1]
-    # X = pad(primary)
-    # Y = pad(secondary)
-    X = primary
-    Y = secondary
+    parser.add_argument("-mp", "--model_path", default="/Users/fryderykkogl/Dropbox (Partners HealthCare)/DL/Models/"
+                                                       "39-dense-515k-mr-vocal-sweep3-10/model_epoch7_valacc0.910.pt",
+                        help="Path to the trained .pt model file")
+    parser.add_argument("-fvp", "--fixed_volume_path", default="/Users/fryderykkogl/Dropbox (Partners HealthCare)/DL/"
+                                                               "Experiments/mr patch convergence/data/49.npy",
+                        help="Path to the fixed .npy volume")
+    parser.add_argument("-ovp", "--offset_volume_path", default="/Users/fryderykkogl/Dropbox (Partners HealthCare)/DL/"
+                                                                "Experiments/mr patch convergence/data/50_49.npy",
+                        help="Path to the offset .npy volume")
 
-    # Solve the least squares problem X * A = Y
-    # to find our transformation matrix A
-    A, res, rank, s = np.linalg.lstsq(X, Y)
+    args = parser.parse_args()
 
-    transform = lambda x: unpad(np.dot(pad(x), A))
+    main(args)
 
-    print("Target:")
-    print(secondary)
-    print("Result:")
-    print(transform(primary))
-    print("Max error:", np.abs(secondary - transform(primary)).max())
-
-    A[np.abs(A) < 1e-10] = 0 # set really small values to zero
-    print(A)
-
-
-# least_squares_transform()
-
-# A, res, rank, s = np.linalg.lstsq(primary, secondary)
-# print(A)
-#
-#
-#
-
-# W = [1, 1, 1, 1, 1]
-# # W = [1,1,1]
-# W = np.sqrt(np.diag(W))
-# prim_w = np.dot(W, primary)
-# sec_w = np.dot(secondary.T, W)
-# A_w, _, _, _ = np.linalg.lstsq(prim_w, sec_w.T)
-# print(A_w.T)
-# print("\n")
-
-
-W = [1., 1.0, 1, 1.0, 1.0]
-W = np.sqrt(np.diag(W))
-prim_w = np.dot(W, primary)
-sec_w = np.dot(secondary.T, W)
-A_w, _, _, _ = np.linalg.lstsq(prim_w, sec_w.T)
-print(A_w.T)
-print("\n")
-
-
-print(utils.calculate_affine_transform(primary_n, secondary_n, [0.01, 1.0, 1.0, 1.0, 1.0]))
-
-
-X = primary
-Y = secondary
-W = [0.1, 0.9, 0.9, 0.9, 0.9]
-W = np.sqrt(np.diag(W))
-a = np.linalg.inv(np.matmul(X.T, np.matmul(W, X)))
-c = np.matmul(X.T, np.matmul(W, Y))
-beta = np.matmul(a, c)
-
-print(beta.T)
-
-
-# wls = LinearRegression()
-# W = [1, 1, 1, 1]
-# wls.fit(primary, secondary)  # , sample_weight=W)
-# print(wls.coef_)
-
-print(5)
